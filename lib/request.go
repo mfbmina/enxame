251eb545e2f1abaf77fcf9c11bfdc5a61c2d1d72
Matheus Mina <mfbmina@gmail.com>
2023-11-19 17:39:28 -0300

    add elapsedtime

--- lib/request.go
+++ lib/request.go
@@ -3,11 +3,18 @@ package lib
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type Response struct {
+	StatusCode int
+	Time       time.Duration
+	Path       string
+}
 
 func Swarm(path string, requestsPerUser int, concurrentUsers int) {
 	totalRequests := requestsPerUser * concurrentUsers
-	channel := make(chan http.Response, totalRequests)
+	channel := make(chan Response, totalRequests)
 
 	fmt.Println("Calling path", path, "...")
 	for i := 0; i < concurrentUsers; i++ {
@@ -16,20 +23,23 @@ func Swarm(path string, requestsPerUser int, concurrentUsers int) {
 
 	for i := 0; i < totalRequests; i++ {
 		resp := <-channel
-		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.StatusCode, resp.Time, resp.Path)
 	}
 
 	fmt.Println("Done!")
 }
 
-func userRequests(path string, requestsPerUser int, channel chan http.Response) {
+func userRequests(path string, requestsPerUser int, channel chan Response) {
 	for i := 0; i < requestsPerUser; i++ {
+		startTime := time.Now()
 		resp, err := http.Get(path)
+		elapsedTime := time.Since(startTime) / time.Millisecond
+
 		if err != nil {
-			channel <- http.Response{StatusCode: 0}
+			channel <- Response{StatusCode: 0, Path: path, Time: elapsedTime}
 			continue
 		}
 
-		channel <- *resp
+		channel <- Response{StatusCode: resp.StatusCode, Path: path, Time: elapsedTime}
 	}
 }
