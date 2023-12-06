a00e7260dd24933b6bb6cdd10d768bccd188660a
Matheus Mina <mfbmina@gmail.com>
2023-12-06 17:35:58 -0300

    feat: add integration with Sentry

--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require github.com/spf13/cobra v1.8.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/getsentry/sentry-go v0.25.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/steinfletcher/apitest v1.5.15 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"time"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/mfbmina/enxame/cmd"
 )
 
 func main() {
-	err := cmd.Execute()
+	defer panicRecover()
 
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              "",
+		TracesSampleRate: 1.0,
+	})
+	err = cmd.Execute()
 	if err != nil {
-		log.Fatal(err)
+		sentry.CaptureException(err)
 	}
+}
+
+func panicRecover() {
+	err := recover()
+	if err != nil {
+		fmt.Println("Something weird happened. Please report this to the developers.")
+		sentry.CurrentHub().Recover(err)
+	}
+
+	sentry.Flush(time.Second * 5)
 }
